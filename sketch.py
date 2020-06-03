dae1397ced3755d747289579a6dc49459399e612
1999shaswat <1999shaswat@gmail.com>
2020-06-04 00:07:27 +0530

    Before adding walls

--- sketch.py
+++ sketch.py
@@ -5,57 +5,149 @@ h = 0
 
 
 class Spot:
-    def __init__(self, x, y):
-        self.f = 0
-        self.g = 0
-        self.h = 0
-        self.x = x
-        self.y = y
+    def __init__(self, i, j):
+        self.f = 10**9
+        self.g = 10**9
+        self.i = i
+        self.j = j
+        self.prev = None
 
-    def show(self,col):
+    def show(self, col):
         fill(col)
         no_stroke()
-        rect((self.x*w, self.y*h), w-1, h-1)
+        rect((self.j*h, self.i*w), w-1, h-1)
 
-width=400
-height=400
-cols = 5
-rows = 5
-grid = [[Spot(i,j) for i in range(cols)] for j in range(rows)]
+    def __str__(self):
+        return f'[{self.i},{self.j}] '
+
+
+width = 400
+height = 400
+cols = 25
+rows = 25
+
+grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
 openSet = []
 closedSet = []
+# path = []
 start = None
 end = None
 
 
 def setup():
-    global w,h,start,end,openSet
+    global w, h, start, end, openSet
     size(width, height)
 
     w = width//cols
     h = height//rows
-
+    for lis in grid:
+        for x in lis:
+            print(x, end="")
+        print()
     start = grid[0][0]
-    end = grid[rows-1][cols-1]
+    start.g=0
+    start.f=0
+    end = grid[13][4]
     openSet.append(start)
 
 
-def draw():
-    if len(openSet) > 0:
-        # keep going
-        pass
-    else:
-        #  no soln
-        pass
+def heuristic(a, b):
+    return abs(a.i-b.i)+abs(a.j-b.j)
 
+
+def getneighbors(spot):
+    adder = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    neighbors = []
+    i = spot.i
+    j = spot.j
+    for a, b in adder:
+        a += i
+        b += j
+        if 0 <= a < rows and 0 <= b < cols:
+            neighbors.append(grid[a][b])  # add wall
+    return neighbors
+
+
+def draw():
+    global openSet, closedSet, grid
+    # if len(openSet) > 0:
+    #     # keep going
+    #     pass
+    # else:
+    #     #  no soln
+    #     pass
+    # ..................................
+    # current=None
+    if len(openSet) > 0:
+        # print("while")
+        current = min(openSet, key=lambda node: node.f)
+        print("curr:",current)
+
+        if current == end:
+            no_loop()
+            print("DONE")
+        else:
+            openSet.remove(current)
+            closedSet.append(current)
+            neighbor = getneighbors(current)
+
+            print("neighbor:",end=" ")
+            for spots in neighbor:
+                print(spots,end=" ")
+            print()
+
+            for i in range(len(neighbor)):
+                # print("n",neighbor[i].i,neighbor[i].j)
+                if neighbor[i] in closedSet:
+                    print("continue")
+                    continue
+                tempG = current.g+1
+                print(f"tg{tempG} ng{neighbor[i].g}")
+                if tempG < neighbor[i].g:
+                    print("if1")
+                    neighbor[i].prev = current
+                    neighbor[i].g = tempG
+                    neighbor[i].f = neighbor[i].g + heuristic(neighbor[i], end)
+                    if neighbor[i] not in openSet:
+                        openSet.append(neighbor[i])
+
+            # if neighbor[i] in openSet:
+            #     if tempG < neighbor[i].g:
+            #         neighbor[i].g = tempG
+            #         neighbor[i].f = neighbor[i].g+heuristic(neighbor[i], end)
+            #         neighbor[i].prev = current
+            # else:
+            #     neighbor[i].g = tempG
+            #     neighbor[i].f = neighbor[i].g+heuristic(neighbor[i], end)
+            #     neighbor[i].prev = current
+            #     openSet.append(neighbor[i])
+            # neighbor[i].f = neighbor[i].g+heuristic(neighbor[i], end)
+            # neighbor[i].prev = current
+
+    # ..................................
     background(0)
+
     for rows in grid:
         for spot in rows:
             spot.show(Color(255))
-    for x in openSet:
-        x.show(Color(255,0,0))
-    for x in openSet:
-        x.show(Color(0,255,0))
+
+    for spot in closedSet:
+        spot.show(Color(255, 0, 0))
+
+    for spot in openSet:
+        spot.show(Color(0, 255, 0))
+
+    end.show((Color(121, 123, 255)))
+    if current!=None:
+        temp = current
+        path = [temp]
+        while temp.prev != None:
+            path.append(temp.prev)
+            temp = temp.prev
+
+    for spot in path:
+        spot.show(Color(0, 0, 255))
+
 
 if __name__ == '__main__':
     run()
