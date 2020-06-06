6098e8e4cea054135854cc2f47104690e03f9ce3
1999shaswat <1999shaswat@gmail.com>
2020-06-06 16:56:07 +0530

    Changed to circle

--- sketch.py
+++ sketch.py
@@ -1,5 +1,5 @@
 from p5 import *
-
+import random
 w = 0
 h = 0
 width = 400
@@ -13,6 +13,7 @@ closedSet = []
 start = None
 end = None
 
+
 class Spot:
     def __init__(self, i, j):
         self.f = 10**9
@@ -20,19 +21,31 @@ class Spot:
         self.i = i
         self.j = j
         self.prev = None
+        self.wall = False
+        if random.random() < 0.3:
+            self.wall = True
 
     def show(self, col):
         fill(col)
+        if self.wall:
+            fill(0)
         no_stroke()
-        rect((self.j*h, self.i*w), w-1, h-1)
+        if self.wall:
+            rect((self.j*h, self.i*w), w-1, h-1)
+        else:
+            circle((self.j*h+h/2, self.i*w+w/2),h/2+6,CENTER)
+        # rect((self.j*h, self.i*w), w-1, h-1)
 
     def __str__(self):
         return f'[{self.i},{self.j}] '
 
+
 grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
+
 
 def heuristic(a, b):
     return abs(a.i-b.i)+abs(a.j-b.j)
+
 
 def getneighbors(spot):
     adder = [(1, 0), (-1, 0), (0, 1), (0, -1)]
@@ -43,8 +56,10 @@ def getneighbors(spot):
         a += i
         b += j
         if 0 <= a < rows and 0 <= b < cols:
-            neighbors.append(grid[a][b])  # add wall
+            if grid[a][b].wall != True:
+                neighbors.append(grid[a][b])  # add wall
     return neighbors
+
 
 def setup():
     global w, h, start, end, openSet
@@ -57,15 +72,21 @@ def setup():
     #         print(x, end="")
     #     print()
     start = grid[0][0]
+    end = grid[13][4]
+    start.wall=False
+    end.wall=False
+    
     start.g = 0
     start.f = 0
-    end = grid[13][4]
+
+
     openSet.append(start)
 
-    background(0)
-    for each_row in grid:
-        for spot in each_row:
-            spot.show(Color(255))
+    # background(0)
+    # for each_row in grid:
+    #     for spot in each_row:
+    #         spot.show(Color(255))
+
 
 def draw():
     global openSet, closedSet, grid
@@ -85,6 +106,7 @@ def draw():
         if current == end:
             no_loop()
             print("DONE")
+            return
         else:
             openSet.remove(current)
             closedSet.append(current)
@@ -122,20 +144,23 @@ def draw():
             #     openSet.append(neighbor[i])
             # neighbor[i].f = neighbor[i].g+heuristic(neighbor[i], end)
             # neighbor[i].prev = current
-
+    else:
+        print("NO Solution")
+        no_loop()
+        return
     # ..................................
-    
+
     # background(0)
 
-    # for each_row in grid:
-    #     for spot in each_row:
-    #         spot.show(Color(255))
-
-    for spot in closedSet:
-        spot.show(Color(255, 0, 0))
-
-    for spot in openSet:
-        spot.show(Color(0, 255, 0))
+    for each_row in grid:
+        for spot in each_row:
+            spot.show(Color(255))
+    # begin_shape()
+    # for spot in closedSet:
+    #     spot.show(Color(255, 0, 0))
+    # end_shape()
+    # for spot in openSet:
+    #     spot.show(Color(0, 255, 0))
 
     end.show((Color(121, 123, 255)))
     if current != None:
